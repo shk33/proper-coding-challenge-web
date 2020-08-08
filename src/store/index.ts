dea15894c798fbba08333d10d22d0f064066ff73
shk33 <m.coronel.seg@gmail.com>
2020-08-08 16:37:49 -0500

    Add Todo Interface

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,23 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+interface Todo {
+    id: String,
+    title: String,
+    completed: boolean
+}
+
 interface State {
     foo: boolean;
+    todos: Array<Todo>,
+    newTodo: String
 }
 
 export default new Vuex.Store<State>({
     state: {
         foo: true,
+        todos: [],
+        newTodo: ''
     },
     mutations: {},
     actions: {},
